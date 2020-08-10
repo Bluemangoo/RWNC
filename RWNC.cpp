596ba0f874a9662af12051f7101ffad72fe5b03a
chenfycc <63218454+chenfycc@users.noreply.github.com>
2020-08-10 08:26:56 +0800

    Add files via upload

--- RWNC.cpp
+++ RWNC.cpp
@@ -1,6 +1,10 @@
 #include<windows.h>
 #include<pthread.h>
+#include<string>
+#include<iostream>
 #define KEY_DOWN(VK_NONAME) ((GetAsyncKeyState(VK_NONAME) & 0x8000) ? 1:0)
+using namespace std;
+string command;
 void* con(void* arg)
 {
 	bool onshow=1;
@@ -19,10 +23,20 @@ void* con(void* arg)
 		_sleep(10);
 	}
 } 
-int main()
+int main(int argc,char *argv[])
 {
-	pthread_t tid;  
-    pthread_create(&tid, NULL, con, NULL);
-	system("obr");
+	char aa[1000];
+	int n=0;
+	while(1)
+	{
+		aa[n]=*(argv[1]+n);
+		if(!aa[n])
+			break;
+		n++;
+	}
+	const char *abc = aa;
+	pthread_t tid;
+	pthread_create(&tid, NULL, con, NULL);
+	system(abc);
 	return 0;
 }
